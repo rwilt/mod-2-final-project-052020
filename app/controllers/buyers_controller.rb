5256254097a84292d8da981727bf8baec22aa6ca
Joshua <jykim92@gmail.com>
2020-05-26 16:23:21 -0400

    view folders

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,32 @@
 class BuyersController < ApplicationController
+
+
+    def index
+        @buyers = Buyer.all
+    end
+
+    def show
+        @buyer = Buyer.find(params[:id])
+    end
+    
+    def new
+        
+    end
+    
+    def create
+    
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def destroy
+    
+    end
+
 end

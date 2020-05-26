3389f021dd51b55cdbccd8a29e587ba09465b205
Joshua <jykim92@gmail.com>
2020-05-26 17:32:26 -0400

    buyer, buyerscart, sellerscart

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,5 @@
 class BuyersController < ApplicationController
+    before_action :find_buyer, only: [:show, :edit, :update, :destroy]
 
 
     def index
@@ -6,27 +7,50 @@ class BuyersController < ApplicationController
     end
 
     def show
-        @buyer = Buyer.find(params[:id])
     end
     
     def new
-        
+        @buyer = Buyer.new
     end
     
     def create
-    
+        @buyer = Buyer.create(buyer_params)
+
+        if @buyer.valid?
+            redirect_to buyer_path(@buyer)
+        else
+            flash[:errors] = @buyer.errors.full_messages
+            redirect_to new_buyer_path
+        end
     end
     
     def edit
-    
     end
     
     def update
-    
+
+        if @buyer.update(buyer_params)
+            redirect_to buyer_path(@buyer)
+        else
+            flash[:errors] = @buyer.errors.full_messages
+            redirect_to edit_buyer_path
+        end
     end
     
     def destroy
-    
+        @buyer.destroy
+        redirect_to buyers_path
+    end
+
+
+    private
+
+    def buyer_params
+        params.require(:buyer).permit(:name, :phone, :zip, :email)
+    end
+
+    def find_buyer
+        @buyer = Buyer.find(params[:id])
     end
 
 end
--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,4 +1,6 @@
 class Buyer < ApplicationRecord
     has_many :buyers_carts
     has_many :items, through: :buyers_carts
+
+    validates :zip, length: { is: 5 }
 end

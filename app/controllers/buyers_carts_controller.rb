6d21915dd054eb9e2a00d8c793dcf7e0e11088ef
Joshua <jykim92@gmail.com>
2020-05-27 09:09:21 -0400

    Buyers + Items

--- app/controllers/buyers_carts_controller.rb
+++ app/controllers/buyers_carts_controller.rb
@@ -1,2 +1,54 @@
 class BuyersCartsController < ApplicationController
+    before_action :find_buyerscart, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @buyerscarts = BuyersCart.all
+    end
+
+    def show
+    end
+    
+    def new
+        @buyerscart = BuyersCart.new
+    end
+    
+    def create
+        @buyerscart = BuyersCart.create(buyers_carts_params)
+
+        if @buyerscart.valid?
+            redirect_to buyers_cart_path(@buyerscart)
+        else
+            flash[:errors] = @buyerscart.errors.full_messages
+            redirect_to new_buyers_cart_path
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        
+        if @buyerscart.update
+            redirect_to buyers_cart_path(@buyerscart)
+        else
+            flash[:errors] = @buyerscart.errors.full_messages
+            redirect_to edit_buyers_cart_path
+        end
+    end
+    
+    def destroy
+        @buyerscart.destroy
+        redirect_to buyers_carts_path
+    end
+
+
+    private
+    def buyers_carts_params
+        params.require(:buyers_cart).permit(:buyer_id)
+    end
+
+    def find_buyerscart
+        @buyerscart = BuyersCart.find(params[:id])
+    end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,60 @@
 class ItemsController < ApplicationController
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @items = Item.all
+    end
+        
+    def show
+    end
+    
+    def new
+        @item = Item.new
+    end
+    
+    def create
+        @item = Item.create(item_params)
+
+        if @item.valid?
+            
+            redirect_to item_path(@item)
+        else
+            flash[:errors] = @item.errors.full_messages
+
+            redirect_to new_item_path
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+    
+        if @item.update(item_params)
+
+            redirect_to item_path(@item)
+        else
+            flash[:errors] = @item.errors.full_messages
+
+            redirect_to edit_item_path(@item)
+        end
+    end
+    
+    def destroy
+        @item.destroy
+        
+        redirect_to items_path
+    end
+
+    
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price, :description, :photo)
+    end
+
+    def find_item
+        @item = Item.find(params[:id])
+    end
+
 end
